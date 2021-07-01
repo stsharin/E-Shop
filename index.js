34c2d242ffbde30f140c6f5c944c3f7233dcb25f
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-01 22:32:58 +0600

    added api

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ client.connect(err => {  // console.log(err)
     })
   })
 
-  app.delete('/delete/:id', (req, res)=>{
+  app.delete('/deleteRegistration/:id', (req, res)=>{
     const id = req.params.id;
     registrationCollection.deleteOne({_id: ObjectId(id)}, (result)=> {
       console.log(result.deleteCount);
